5ce9b2c64b1aeda4a799827a8070176f6fc9fa4f
Alex Grinman <agrinman@users.noreply.github.com>
2020-04-12 18:53:55 -0400

    Update wormhole.rb

--- wormhole.rb
+++ wormhole.rb
@@ -2,7 +2,7 @@ class Wormhole < Formula
   desc "expose your local web server to the internet with a public url"
   homepage "https://github.com/agrinman/wormhole"
   url "https://github.com/agrinman/wormhole/archive/0.1.2.zip"
-  sha256 "6856e20c0e6450429ca0a327d2630dfcffe68eab2a9f2fefe8fef546eb741714"
+  sha256 "28c89eae7ecb2bc039a413d702a97bd348f5d4991e1a4369ff00a24063387994"
 
   depends_on "rust" => :build
 
@@ -12,7 +12,7 @@ class Wormhole < Formula
 
   bottle do
     root_url "https://github.com/agrinman/wormhole/releases/download/0.1.2"
-    sha256 "0957f125a9eb32971c58f779dc319894d1c84f67470d6fa886daa3eee0154522" => :catalina
+    sha256 "dea10e60a5b51f50db227619f45be2a0ba935015c76f4c527a81c4b33186a914" => :catalina
   end
 
   test do
