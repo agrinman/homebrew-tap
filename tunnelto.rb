1a67e5953b38e5665c1eaf8da1c2c3c7bd3a3a54
Alex Grinman <agrinman@users.noreply.github.com>
2020-07-24 21:07:54 -0400

    Update tunnelto.rb

--- tunnelto.rb
+++ tunnelto.rb
@@ -1,8 +1,8 @@
 class Tunnelto < Formula
   desc "Expose your local web server to the internet with a public url: https://tunnelto.dev"
   homepage "https://github.com/agrinman/tunnelto"
-  url "https://github.com/agrinman/tunnelto/archive/0.1.11.zip"
-  sha256 "5441fcb63443da2e16fe06ca66df47b1fafb9f7fd064fc4160a79147f376aec7"
+  url "https://github.com/agrinman/tunnelto/archive/0.1.12.zip"
+  sha256 "a72d7db1aa9c4688f937e16de23107c161e82162f39dc3f32880a0fc9f73574b"
 
   depends_on "rust" => :build
 
@@ -11,9 +11,9 @@ class Tunnelto < Formula
   end
 
   bottle do
-    root_url "https://github.com/agrinman/tunnelto/releases/download/0.1.11"
-    sha256 "43a65fa1f4a3a2703f56f9c4b4d6ee80e130916a5b335155c46e3f31d755d041" => :catalina
-    sha256 "43a65fa1f4a3a2703f56f9c4b4d6ee80e130916a5b335155c46e3f31d755d041" => :big_sur    
+    root_url "https://github.com/agrinman/tunnelto/releases/download/0.1.12"
+    sha256 "a92156531a8062b3f0bc1dd57a92f1fa3cb674051ba426b70bb2ab73f21f479a" => :catalina
+    sha256 "a92156531a8062b3f0bc1dd57a92f1fa3cb674051ba426b70bb2ab73f21f479a" => :big_sur    
   end
 
   test do
